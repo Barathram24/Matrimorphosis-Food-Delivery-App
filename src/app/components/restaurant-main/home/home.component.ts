fbb866bd99260e86be2183839a02a542cf77cffe
Barathram46 <barathram764@gmail.com>
2025-04-20 13:33:34 +0530

    home

--- src/app/components/restaurant-main/home/home.component.ts
+++ src/app/components/restaurant-main/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [RouterModule],
+  standalone: true
 })
+
 export class HomeComponent {
 
   constructor(private router: Router) {}
